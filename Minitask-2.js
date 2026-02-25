422d38fcf7b1d7517e5943ab689f0d6696dc662d
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-25 14:31:38 +0700

    Add discount calculation function
    Implement discount calculation based on total amount.

---
+++ Minitask-2.js
@@ -0,0 +1,19 @@
+//Case Discount Belanja
+let total = 10000000
+function calculateDiscount(total){
+  let result = 0
+  if(total>=0){
+     if(total>1000000){
+       result = total - (total*(10/100))
+     }else if(total>500000){
+       result = total - (total * (5/100))
+     }else{
+       result = total
+     }
+   }else{
+     return console.log("Nominal invalid")
+   }
+  return result
+}
+
+calculateDiscount(total)
